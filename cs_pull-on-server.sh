f2dc7e3fe8421f3a20042096cf3afdf8777ff133
juanitolaguna <juanitolaguna@protonmail.com>
2021-09-30 14:37:26 +0200

    add scripts

--- cs_pull-on-server.sh
+++ cs_pull-on-server.sh
@@ -2,7 +2,7 @@
 
 . .env
 
-ssh $USER@$REMOTE 'git -C '$PLUGINPATH' pull'
+ssh $USER@$REMOTE 'git -C '$PLUGINPATH' pull origin main'
 #copies the js, makes migrations, executes update()
 ssh $USER@$REMOTE 'sudo -u www-data '$BINPATH'/bin/console plugin:update '$PLUGINNAME
 
@@ -11,6 +11,3 @@ ssh $USER@$REMOTE 'sudo -u www-data '$BINPATH'/bin/console cache:clear'
 
 # execute with www-data as user
 ssh $USER@$REMOTE  'sudo -u www-data '$BINPATH'/bin/console theme:compile'
-
-
-
